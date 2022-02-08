d916538e2c155d05795ba9fbe3ae2af64949b97a
Kristopher Werlinder <42782387+Christofferos@users.noreply.github.com>
2022-02-08 14:04:18 +0100

    Handling file reader in case file is not found

--- server/src/firestore.js
+++ server/src/firestore.js
@@ -5,24 +5,31 @@ import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const serviceAccount = JSON.parse(fs.readFileSync('./privateFirestoreKey.json', 'utf-8'));
-const serviceAccountEnv = !serviceAccount
-  ? {
-      type: 'service_account',
-      project_id: 'chessonlinepro',
-      private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY,
-      client_email: 'firebase-adminsdk-l8nkl@chessonlinepro.iam.gserviceaccount.com',
-      client_id: process.env.CLIENT_ID,
-      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-      token_uri: 'https://oauth2.googleapis.com/token',
-      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-      client_x509_cert_url:
-        'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l8nkl%40chessonlinepro.iam.gserviceaccount.com',
-    }
-  : null;
+let serviceAccount;
+let privateFirestoreKey;
+try {
+  privateFirestoreKey = fs.readFileSync('./privateFirestoreKey.json', 'utf-8');
+  serviceAccount = JSON.parse(privateFirestoreKey);
+} catch (err) {
+  serviceAccount = null;
+}
+if (!serviceAccount) {
+  serviceAccount = {
+    type: 'service_account',
+    project_id: 'chessonlinepro',
+    private_key_id: process.env.PRIVATE_KEY_ID || '',
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n') || '',
+    client_email: 'firebase-adminsdk-l8nkl@chessonlinepro.iam.gserviceaccount.com',
+    client_id: process.env.CLIENT_ID || '',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+      'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l8nkl%40chessonlinepro.iam.gserviceaccount.com',
+  };
+}
 initializeApp({
-  credential: cert(serviceAccount ?? serviceAccountEnv),
+  credential: cert(serviceAccount),
 });
 const firestore = getFirestore();
 
