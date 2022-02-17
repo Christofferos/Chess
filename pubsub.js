80bf78f5e49eff8eee2219ea7524be98368e939e
Kristopher Werlinder <42782387+Christofferos@users.noreply.github.com>
2022-02-17 10:13:34 +0100

    Test code

--- pubsub.js
+++ pubsub.js
@@ -2,6 +2,6 @@ import { PubSub } from "@google-cloud/pubsub";
 import path from "path";
 
 const projectId = "chessonlinepro";
-const keyFilename = path.join(path.resolve(), "google-credentials.json");
+const keyFilename = path.join(path.resolve(), "..", "google-credentials.json");
 export const pubSubClient = new PubSub({ projectId, keyFilename });
 export const TOPIC_NAME = "projects/chessonlinepro/topics/signup";
